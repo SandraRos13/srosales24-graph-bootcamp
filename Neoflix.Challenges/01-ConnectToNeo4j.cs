c1bdbd69c26805accca07c2559e4ae74072ea7ab
SandraRos13 <ssra.uami@gmail.com>
2024-02-29 13:59:34 -0800

    Cambios

--- Neoflix.Challenges/01-ConnectToNeo4j.cs
+++ Neoflix.Challenges/01-ConnectToNeo4j.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Threading.Tasks;
+using Neo4j.Driver;
 using NUnit.Framework;
 
 namespace Neoflix.Challenges
@@ -9,7 +11,8 @@ namespace Neoflix.Challenges
 
         [Test, Order(1)]
         public async Task InitDriverAsync_should_create_driver_and_connect_to_server()
-        {
+        {    
+            //var (uri, username, password) = Config.UnpackNeo4jConfig();
             var (uri, username, password) = Config.UnpackNeo4jConfig();
 
             Assert.NotNull(uri);
@@ -17,24 +20,34 @@ namespace Neoflix.Challenges
             Assert.NotNull(password);
 
             await Neo4j.InitDriverAsync(uri, username, password);
+            
         }
 
         [Test, Order(2)]
-        public void Driver_should_have_been_instantiated()
+
+        public async void Driver_should_have_been_instantiated()
         {
             var driver = Neo4j.Driver;
+            InitDriverAsync_should_create_driver_and_connect_to_server.Run();
+            //string username="neo4j";
+            //string password="wastes-masts-hotels";
+            //string uri="bolt://3.94.149.46:7687";
+            //GraphDatabase.Driver(uri,AuthTokens.Basic(username,password));
+            //InitDriverAsync_should_create_driver_and_connect_to_server();
+            Assert.NotNull(driver);   
+            //await Neo4j.InitDriverAsync(uri, user, password);         
 
-            Assert.NotNull(driver);
         }
 
         [Test, Order(3)]
         public async Task Driver_should_be_able_to_verify_connectivity()
         {
             var driver = Neo4j.Driver;
-
+            //Console.WriteLine("Connection Verified!");
             Assert.NotNull(driver);
 
             await driver.VerifyConnectivityAsync();
+            
         }
     }
 }
